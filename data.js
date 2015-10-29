3ff49ad7faac81d3657f38eb9839ed16c61819b0
andjosh <email@andjosh.com>
2015-10-28 20:58:04 -0500

    Working, just need some data

--- data.js
+++ data.js
@@ -1,3 +1,6 @@
-window.data =
-[1,1,1,1,1,1,1,1,1,1,1,1,11,1,1,1,1,1,1,1,1,1
+window.data = [
+    {html: 'of a black garden ant body.', dist: 0.6}
+    ,{html: 'of a carpenter ant body.', dist: 2.5}
+    ,{html: 'of the average human optic nerve.', dist: 5}
+    ,{html: 'of the longest human tongue.', dist: 9.8044}
 ];
